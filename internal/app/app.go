d1bb4d6f82bc6bf9a5782179e8fd74e59eb15e78
artemiikulikov <aapqwhat@gmail.com>
2024-11-12 01:47:35 +0300

    feat: add a bunch of crazy mock code and etc..

--- internal/app/app.go
+++ internal/app/app.go
@@ -9,7 +9,9 @@ import (
 	"github.com/curtrika/UMetrika_server/internal/domain/models"
 	"github.com/curtrika/UMetrika_server/internal/services/admin_panel"
 	"github.com/curtrika/UMetrika_server/internal/services/auth"
+	storage "github.com/curtrika/UMetrika_server/internal/storage/sqlc_gen"
 	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5/pgtype"
 )
 
 type App struct {
@@ -20,7 +22,30 @@ type Repository interface {
 	SaveUser(ctx context.Context, email string, passHash []byte) (uuid.UUID, error)
 	GetUserByEmail(ctx context.Context, email string) (*models.User, error)
 	// SaveApp(ctx context.Context, app models.App) (uuid.UUID, error)
+
+	// currently its easier to import storage models than to manualy copy-paste them to model packaeg
 	GetAppById(ctx context.Context, appID int32) (*models.App, error)
+	CreateAnswer(ctx context.Context, arg storage.CreateAnswerParams) (storage.Answer, error)
+	CreateApp(ctx context.Context, arg storage.CreateAppParams) (storage.App, error)
+	CreatePsychologicalPerformance(ctx context.Context, arg storage.CreatePsychologicalPerformanceParams) (storage.PsychologicalPerformance, error)
+	CreatePsychologicalTest(ctx context.Context, arg storage.CreatePsychologicalTestParams) (storage.PsychologicalTest, error)
+	CreatePsychologicalType(ctx context.Context, title string) (storage.PsychologicalType, error)
+	CreateQuestion(ctx context.Context, arg storage.CreateQuestionParams) (storage.Question, error)
+	CreateUser(ctx context.Context, arg storage.CreateUserParams) (storage.User, error)
+	GetAnswer(ctx context.Context, id int32) (storage.Answer, error)
+	GetApp(ctx context.Context, id int32) (storage.App, error)
+	GetPsychologicalPerformance(ctx context.Context, id int32) (storage.PsychologicalPerformance, error)
+	GetPsychologicalTest(ctx context.Context, id int32) (storage.PsychologicalTest, error)
+	GetPsychologicalType(ctx context.Context, id int32) (storage.PsychologicalType, error)
+	GetQuestion(ctx context.Context, id int32) (storage.Question, error)
+	GetUser(ctx context.Context, id pgtype.UUID) (storage.User, error)
+	ListAnswers(ctx context.Context) ([]storage.Answer, error)
+	ListApps(ctx context.Context) ([]storage.App, error)
+	ListPsychologicalPerformances(ctx context.Context) ([]storage.PsychologicalPerformance, error)
+	ListPsychologicalTests(ctx context.Context) ([]storage.PsychologicalTest, error)
+	ListPsychologicalTypes(ctx context.Context) ([]storage.PsychologicalType, error)
+	ListQuestions(ctx context.Context) ([]storage.Question, error)
+	ListUsers(ctx context.Context) ([]storage.User, error)
 }
 
 func Init(
--- internal/grpc/admin_panel/server.go
+++ internal/grpc/admin_panel/server.go
@@ -8,6 +8,9 @@ import (
 	adminpanelv1 "github.com/curtrika/UMetrika_server/pkg/proto/admin_panel/v1"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
 	"google.golang.org/grpc/credentials/insecure"
 )
 
@@ -50,4 +53,68 @@ func RunRest(ctx context.Context) {
 
 func (s *serverAPI) Ping(ctx context.Context, req *adminpanelv1.PingMessage) (*adminpanelv1.PingMessage, error) {
 	return req, nil
+}
+
+func (s *serverAPI) CreateDiscipline(context.Context, *adminpanelv1.CreateDisciplineRequest) (*adminpanelv1.CreateDisciplineResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method CreateDiscipline not implemented")
+}
+
+func (s *serverAPI) CreatePsychologicalType(context.Context, *adminpanelv1.CreatePsychologicalTypeRequest) (*adminpanelv1.PsychologicalTypeResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method CreatePsychologicalType not implemented")
+}
+
+func (s *serverAPI) GetPsychologicalType(context.Context, *adminpanelv1.GetPsychologicalTypeRequest) (*adminpanelv1.PsychologicalTypeResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetPsychologicalType not implemented")
+}
+
+func (s *serverAPI) ListPsychologicalTypes(context.Context, *adminpanelv1.ListPsychologicalTypesRequest) (*adminpanelv1.ListPsychologicalTypesResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ListPsychologicalTypes not implemented")
+}
+
+func (s *serverAPI) CreatePsychologicalTest(context.Context, *adminpanelv1.CreatePsychologicalTestRequest) (*adminpanelv1.PsychologicalTestResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method CreatePsychologicalTest not implemented")
+}
+
+func (s *serverAPI) GetPsychologicalTest(context.Context, *adminpanelv1.GetPsychologicalTestRequest) (*adminpanelv1.PsychologicalTestResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetPsychologicalTest not implemented")
+}
+
+func (s *serverAPI) ListPsychologicalTests(context.Context, *adminpanelv1.ListPsychologicalTestsRequest) (*adminpanelv1.ListPsychologicalTestsResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ListPsychologicalTests not implemented")
+}
+
+func (s *serverAPI) CreateQuestion(context.Context, *adminpanelv1.CreateQuestionRequest) (*adminpanelv1.QuestionResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method CreateQuestion not implemented")
+}
+
+func (s *serverAPI) GetQuestion(context.Context, *adminpanelv1.GetQuestionRequest) (*adminpanelv1.QuestionResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetQuestion not implemented")
+}
+
+func (s *serverAPI) ListQuestions(context.Context, *adminpanelv1.ListQuestionsRequest) (*adminpanelv1.ListQuestionsResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ListQuestions not implemented")
+}
+
+func (s *serverAPI) CreateAnswer(context.Context, *adminpanelv1.CreateAnswerRequest) (*adminpanelv1.AnswerResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method CreateAnswer not implemented")
+}
+
+func (s *serverAPI) GetAnswer(context.Context, *adminpanelv1.GetAnswerRequest) (*adminpanelv1.AnswerResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetAnswer not implemented")
+}
+
+func (s *serverAPI) ListAnswers(context.Context, *adminpanelv1.ListAnswersRequest) (*adminpanelv1.ListAnswersResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ListAnswers not implemented")
+}
+
+func (s *serverAPI) CreatePsychologicalPerformance(context.Context, *adminpanelv1.CreatePsychologicalPerformanceRequest) (*adminpanelv1.PsychologicalPerformanceResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method CreatePsychologicalPerformance not implemented")
+}
+
+func (s *serverAPI) GetPsychologicalPerformance(context.Context, *adminpanelv1.GetPsychologicalPerformanceRequest) (*adminpanelv1.PsychologicalPerformanceResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetPsychologicalPerformance not implemented")
+}
+
+func (s *serverAPI) ListPsychologicalPerformances(context.Context, *adminpanelv1.ListPsychologicalPerformancesRequest) (*adminpanelv1.ListPsychologicalPerformancesResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ListPsychologicalPerformances not implemented")
 }
